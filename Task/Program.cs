2722765933bb70e25420d85c057e4f4d08201ff6
vasiltsov <vnigga@list.ru>
2023-01-17 03:03:27 +0300

    Added 1and2 array, PrintArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//лучше обойтись исключительно массивами.
+
+string[] FirstArray = new string[5] {"Hello", "2", "world", "1234", ":-)"};
+string[] SecondArray = new string[FirstArray.Length];
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
